fea0edcb6c0f100b47c3d93d58c8223b8f931b50
Ollie Relph <ollie@relph.me>
2015-11-11 19:31:16 +0000

    Initial Android Support

---
+++ index.android.js
@@ -0,0 +1,45 @@
+import React, {
+  Component,
+  View,
+} from 'react-native';
+import CardIO, { CardIOConstants } from 'react-native-card.io';
+
+
+class CardIOExample extends Component {
+
+  render() {
+    return (
+      <View
+        style={
+          {
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }
+        }
+      >
+        <CardIO
+          style={
+            {
+              flex: 1,
+              backgroundColor: 'black',
+            }
+          }
+          hideCardIOLogo={ true }
+          detectionMode={ CardIOConstants.DETECTION_MODE.AUTOMATIC }
+          onSuccess={
+            ( cardInfo ) => console.log(cardInfo)
+          }
+          onFailure={
+            ( err ) => console.error(err)
+          }
+        />
+      </View>
+    );
+  }
+
+}
+
+AppRegistry.registerComponent('CardIOExample', () => CardIOExample);
--- src/main.js
+++
@@ -1,4 +0,0 @@
-import React from 'react-native';
-import App from './containers/App.jsx';
-
-React.AppRegistry.registerComponent('BBBCardIOExample', () => App);
